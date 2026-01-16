ef05ea659c59cb25c7a3bc2d21f82d26a5f4b0e0
Eleazar Celis <francisco.celish@gmail.com>
2026-01-15 19:18:13 -0600

    feat: Enhance DateBar component with localization and date formatting improvements

--- src/ui/components/DateBar.tsx
+++ src/ui/components/DateBar.tsx
@@ -1,6 +1,8 @@
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs from "dayjs";
+import es from "dayjs/locale/es";
 import {
   getCurrentDate,
   addDayToDate,
@@ -8,12 +10,12 @@ import {
   getDayName,
   getTargetDate,
 } from "../utils/utils";
-import { Box, Button, IconButton, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import ArrowBackIosOutlinedIcon from "@mui/icons-material/ArrowBackIosOutlined";
 import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
-import { Height } from "@mui/icons-material";
 import { useAgendaContext } from "../contexts/AgendaContext";
 import styled from "@emotion/styled";
+import type { Dayjs } from "dayjs";
 
 const StyledButton = styled(Button)({
   color: "#ffff",
@@ -44,51 +46,36 @@ const styles = {
 };
 
 export default function DateBar() {
-  // const now = dayjs()
-  const { dateObj, formattedDate } = getCurrentDate();
   const { agendaData, setAgendaData } = useAgendaContext();
   const { fecha } = agendaData;
-
-  const nextday = addDayToDate(dateObj.format("DD-MM-YYYY"));
-  const prevday = subtractDayToDate(dateObj.format("DD-MM-YYYY"));
   const now = getTargetDate(fecha);
   const today = now.get("date");
   const wDay = getDayName(now.get("day"));
   const month = now.get("month");
   const year = now.get("year");
-  // console.log({ today, month, year, nextday, prevday, dateObj, wDay });
 
-  const handlePrevDay = () => {
-    const newDate = subtractDayToDate(fecha);
+  const updateDate = (newDate: string) => {
     setAgendaData({
       ...agendaData,
-      fecha: newDate.format("DD-MM-YYYY"),
+      fecha: newDate,
     });
+  };
+
+  const handlePrevDay = () => {
+    updateDate(subtractDayToDate(fecha).format("DD-MM-YYYY"));
   };
 
   const handleToday = () => {
-    const { formattedDate } = getCurrentDate();
-    setAgendaData({
-      ...agendaData,
-      fecha: formattedDate,
-    });
+    updateDate(getCurrentDate().formattedDate);
   };
 
   const handleNextDay = () => {
-    const newDate = addDayToDate(fecha);
-    setAgendaData({
-      ...agendaData,
-      fecha: newDate.format("DD-MM-YYYY"),
-    });
+    updateDate(addDayToDate(fecha).format("DD-MM-YYYY"));
   };
 
-  const handleChangeDate = (newValue: any) => {
+  const handleChangeDate = (newValue: Dayjs | null) => {
     if (newValue) {
-      const selectedDate = newValue.format("DD-MM-YYYY");
-      setAgendaData({
-        ...agendaData,
-        fecha: selectedDate,
-      });
+      updateDate(newValue.format("DD-MM-YYYY"));
     }
   };
 
@@ -111,17 +98,17 @@ export default function DateBar() {
       </Box>
       <Typography
         variant="h6"
-        sx={{ minWidth: "120px", textAlign: "center", textTransform: "none" }}
+        sx={{ minWidth: "120px", textAlign: "center" }}
       >
-        {`${wDay}`}
+        {wDay}
       </Typography>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="es">
         <DatePicker
           views={["year", "month", "day"]}
           openTo="day"
-          format={"MMMM DD, YYYY"}
-          defaultValue={now}
-          value={now}
+          format={"DD/MM/YYYY"}
+          defaultValue={dayjs(now).locale("es")}
+          value={dayjs(now).locale("es")}
           slotProps={{ textField: { sx: styles.textField } }}
           onChange={handleChangeDate}
         />
